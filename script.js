7aa218fcd902ee283c92891a6828edec4f6493f7
DavideCavallotto <davidecavallotto92@gmail.com>
2023-10-23 17:10:35 +0200

    ADD button and class to vue

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ createApp ({
     data() {
         return {
             message: 'Hello Vue!',
-            img: './img/developer.jpg'
+            imgs: 'https://picsum.photos/200/300',
+            className: 'section'
         }
+    },
+    methods: {
+        refreshPage: function () {            
+            window.location.reload()
+        } 
     }
 
 }).mount('#app')
+
+
